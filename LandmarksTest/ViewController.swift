4c9f3dc06afc44108b82520de7da117dd46ad839
Sergey <Starcom12@mail.ru>
2021-06-24 22:07:12 +0300

    first commit

---
+++ LandmarksTest/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  LandmarksTest
+//
+//  Created by Гнатюк Сергей on 24.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
